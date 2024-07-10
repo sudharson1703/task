8c1284b381a030c3ae61deeb31c4d0a3c62c7f44
sudharson1703 <sudharsonyuvaraj@gmail.com>
2024-07-10 21:10:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vendors Contact list',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
